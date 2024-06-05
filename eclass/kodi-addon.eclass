kodi-overlay/kodi-overlay

commit b7b9953da4f65e2db15f01073d3a6c8ca7b19540

kodi-addon.eclass: add handling for live packages
Signed-off-by: Alfred Wingate <parona@protonmail.com>

eclass/kodi-addon.eclass
@@ -52,16 +52,21 @@ esac
 # @PRE_INHERIT
 # @DESCRIPTION:
 # Name for the kodi addon Github source tag name.
+# Not checked with *9999 versions.
 # Example: 21.0.1-Omega
 
-KODI_ADDON_TAG="${KODI_ADDON_TAG:=${PV}-${CODENAME}}"
+if [[ "${PV}" =~ *9999 ]]; then
+	inherit git-r3
+	EGIT_REPO_URI="https://github.com/${KODI_GH_ORG}.git"
+else
+	KODI_ADDON_TAG="${KODI_ADDON_TAG:=${PV}-${CODENAME}}"
 
-SRC_URI="
-	https://github.com/${KODI_GH_ORG}/${KODI_ADDON_PN}/archive/${KODI_ADDON_TAG}.tar.gz
-		-> ${P}.tar.gz
-"
-
-S="${WORKDIR}/${KODI_ADDON_PN}-${KODI_ADDON_TAG}"
+	SRC_URI="
+		https://github.com/${KODI_GH_ORG}/${KODI_ADDON_PN}/archive/${KODI_ADDON_TAG}.tar.gz
+			-> ${P}.tar.gz
+	"
+	S="${WORKDIR}/${KODI_ADDON_PN}-${KODI_ADDON_TAG}"
+fi
 
 # @ECLASS_VARIABLE: CODENAME
 # @PRE_INHERIT

commit 981e89784d651424cd10abd578f3b2e45e3857cf

kodi-addon.eclass: unset BASH_REMATCH instead of using case
* [[ =~ ]] declares BASH_REMATCH leading to pkgcheck complaining about it.

"Temporary variables should be unset after use so they aren't exported."

* To address this unset it explicitly after.

Fixes: b7b9953da4f65e2db15f01073d3a6c8ca7b19540
Reverts: c6ffda8c4ec1eb655e152529f38e58d303ab3c0d
Signed-off-by: Alfred Wingate <parona@protonmail.com>

eclass/kodi-addon.eclass
@@ -72,24 +72,22 @@ case ${PN} in
 	;;
 esac
 
-case ${PV} in
-	*9999)
-		inherit git-r3
-		EGIT_REPO_URI="https://github.com/${KODI_GH_ORG}/${KODI_ADDON_PN}.git"
-		if [[ "${PV}" =~ 21* ]]; then
-			EGIT_BRANCH="Omega"
-		fi
-	;;
-	*)
-		KODI_ADDON_TAG="${KODI_ADDON_TAG:=${PV}-${CODENAME}}"
+if [[ ${PV} =~ 9999$ ]]; then
+	inherit git-r3
+	EGIT_REPO_URI="https://github.com/${KODI_GH_ORG}/${KODI_ADDON_PN}.git"
+	if [[ "${PV}" =~ 21* ]]; then
+		EGIT_BRANCH="Omega"
+	fi
+else
+	KODI_ADDON_TAG="${KODI_ADDON_TAG:=${PV}-${CODENAME}}"
 
-		SRC_URI="
-			https://github.com/${KODI_GH_ORG}/${KODI_ADDON_PN}/archive/${KODI_ADDON_TAG}.tar.gz
-				-> ${P}.tar.gz
-		"
-		S="${WORKDIR}/${KODI_ADDON_PN}-${KODI_ADDON_TAG}"
-	;;
-esac
+	SRC_URI="
+		https://github.com/${KODI_GH_ORG}/${KODI_ADDON_PN}/archive/${KODI_ADDON_TAG}.tar.gz
+			-> ${P}.tar.gz
+	"
+	S="${WORKDIR}/${KODI_ADDON_PN}-${KODI_ADDON_TAG}"
+fi
+unset BASH_REMATCH
 
 case ${CODENAME} in
 	Omega)
kodi-overlay/kodi-overlay

commit 5a0d30b7d312541b305c451f5af31996da5ce649

media-tv/kodi: rapidjson -> nlohmann_json
Closes: https://bugs.gentoo.org/955507
Signed-off-by: Alfred Wingate <parona@protonmail.com>

media-tv/kodi/kodi-9999.ebuild
@@ -245,7 +245,7 @@ RDEPEND="
 DEPEND="
 	${COMMON_DEPEND}
 	${COMMON_TARGET_DEPEND}
-	>=dev-libs/rapidjson-1.0.2
+	>=dev-cpp/nlohmann_json-3.2.0
 	test? (
 		>=dev-cpp/gtest-1.10.0
 	)

commit 116a5c758a62d9d1eeab86b05a76189986b68ac1

media-tv/kodi: also change cmake variable
Fixes: 5a0d30b7d312541b305c451f5af31996da5ce649
Signed-off-by: Alfred Wingate <parona@protonmail.com>

media-tv/kodi/kodi-9999.ebuild
@@ -403,8 +403,8 @@ src_configure() {
 		-DENABLE_INTERNAL_FMT=OFF
 		-DENABLE_INTERNAL_FSTRCMP=OFF
 		-DENABLE_INTERNAL_GTEST=OFF
+		-DENABLE_INTERNAL_NLOHMANNJSON=OFF
 		-DENABLE_INTERNAL_PCRE2=OFF
-		-DENABLE_INTERNAL_RapidJSON=OFF
 		-DENABLE_INTERNAL_SPDLOG=OFF
 		-DENABLE_INTERNAL_TAGLIB=OFF
 		-DENABLE_INTERNAL_UDFREAD=OFF